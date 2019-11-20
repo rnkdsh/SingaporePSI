6055783ecb5d117f4c306a7c1c191c4d4f9abe63
Ronak Doshi <ronakdoshi10@gmail.com>
2019-11-20 18:57:55 +0530

    Implement google maps in home screen

--- app/src/main/java/com/singaporepsi/ui/home/HomeFragment.kt
+++ app/src/main/java/com/singaporepsi/ui/home/HomeFragment.kt
@@ -2,16 +2,56 @@ package com.singaporepsi.ui.home
 
 import android.os.Bundle
 import androidx.fragment.app.activityViewModels
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.singaporepsi.R
 import com.singaporepsi.ui.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_home.*
 
-class HomeFragment : BaseFragment(R.layout.fragment_home) {
+class HomeFragment : BaseFragment(R.layout.fragment_home), OnMapReadyCallback {
 
     private val homeViewModel: HomeViewModel by activityViewModels { viewModelFactory }
+    private var map: GoogleMap? = null
 
     override fun getViewModel() = homeViewModel
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        map = p0
+    }
+
+    override fun onResume() {
+        mapView?.onResume()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
     }
 }
